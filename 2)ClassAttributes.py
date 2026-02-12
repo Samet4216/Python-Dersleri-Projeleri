f98be08a86201a6eb5bd949e54a44b7fd6eae922
Samet4216 <sametturkoglu4216@gmail.com>
2026-02-12 17:00:06 +0300

    12.02.26
    Class Attributes

---
+++ 2)ClassAttributes.py
@@ -0,0 +1,29 @@
+"""01.02.26-12.02.26
+Drone adinda bir sinif olustur.
+Class Attributes:
+drone_count: Uretilen her drone ile 1 artan bir sayac.
+operation_code: Tüm filo icin ayni olan bir string (Örn: "ZEYTİN_DALI").
+Instance Attributes:
+unit_id: Her drone'a ozel bir kimlik.
+Metotlar:
+__init__: Her yeni drone eklendiginde drone_count'u artirmali.
+get_fleet_info: Toplam drone sayisini ve operasyon kodunu döndüren bir instance method.
+"""
+
+class drone:
+    drone_count = 0
+    operation_code = "ZEYTİN_DALI" 
+    def __init__(self,id):
+        drone.drone_count +=1 #drone_countun alindigi yeri bildirmek icin drone.drone_count kullanilir.
+        self.id=id
+    def unit_id(self):
+        return self.id[:3].upper()
+    def get_fleet_into(self):
+        return f"The total number of drones is {drone.drone_count}"
+    #dısarıdan drone_count almiyoruz ama f-stringde belirtirken ust classta oldugu bildirilir.
+
+t1=drone("turkiye42")
+t2=drone("konya123")
+t3=drone("ankara789")
+print(t1.unit_id())
+print(t1.get_fleet_into())
---
+++ 2)Class_Attributes.py
@@ -0,0 +1,26 @@
+#1)Class Attributes, dogrudan sinifin icine, metotlarin disina tanimlanir. Bu degiskenler o siniftan uretilen tum nesneler icin ortaktir.
+class Asker:
+    ordu_adi = "TÜRK KARA KUVVETLERİ"  # Class Attribute
+
+    def __init__(self, isim, kilo):
+        self.isim = isim
+        self.kilo = kilo 
+
+#2)Namespace (Isim Alanı): Sinif nitelikleri ClassName.__dict__ icinde tutulurken, instance nitelikleri instance.__dict__ icinde tutulur. 
+#Python bir degiskeni ararken once nesneye (-->self.ordu_adi?), bulamazsa sinifa (-->ordu_adi = "TÜRK KARA KUVVETLERİ") bakar.
+
+#3)Tracking (Takip): Belli bir sinifin icinde uretilen nesnelerin sayisini belirlemede kullanilir.
+
+class IHA:
+    aktif_iha_sayisi = 0 
+
+    def __init__(self, iha_id):
+        self.iha_id = iha_id
+        # Her yeni nesne olusturuldugunda sayaci artiriyoruz.
+        IHA.aktif_iha_sayisi += 1
+        print(f"IHA {self.iha_id} sisteme girdi. Aktif Sayi: {IHA.aktif_iha_sayisi}")
+
+    def __del__(self):
+        # Nesne silindiginde (gorev bittiginde) sayaci azaltiyoruz.
+        IHA.aktif_iha_sayisi -= 1
+        print(f"IHA {self.iha_id} sistemden ayrildi. Kalan Aktif Sayi: {IHA.aktif_iha_sayisi}") 
